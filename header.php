8b00f2d1fd2134e5327c1b8a5dbf653d05e1b55d
Rajesh Reddy Thiyyagura <raj30498@gmail.com>
2023-04-06 17:39:37 -0400

    a

--- header.php
+++ header.php
@@ -1,30 +1,82 @@
-<!-- header file consists of topmenu bar with links to login/register/shop pages -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>My E-commerce Website</title>
-	<!-- Add your CSS and JavaScript files here -->
-	<link rel="stylesheet" href="css/style.css">
+	<title>Project2 Ecommerce website</title>
+	
+	<!-- Add your CSS files here -->
+	<link rel="stylesheet" href="css/styles.css">
+
+	<!-- Add your JavaScript files here -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/main.js"></script>
+
+	<!-- Add any additional meta tags or links here -->
+	<meta name="description" content="Your website description here">
+	
+	<style>
+		/* Add your custom styles here */
+		header {
+			background-color: #8BC34A;
+			padding: 10px 20px;
+			color: white;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+		
+		nav {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+		}
+		
+		nav ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+			display: flex;
+			flex-direction: row;
+		}
+		
+		nav li {
+			margin-right: 20px;
+		}
+		
+		nav li:last-child {
+			margin-right: 0;
+		}
+		
+		nav a {
+			color: white;
+			text-decoration: none;
+			font-size: 18px;
+			font-weight: bold;
+			text-transform: uppercase;
+		}
+		
+		nav a:hover {
+			color: #333;
+		}
+		
+		.logo {
+			font-size: 24px;
+			font-weight: bold;
+			text-transform: uppercase;
+			margin-right: 20px;
+		}
+	</style>
 </head>
 <body>
 	<header>
+		<div class="logo">Project2 Ecommerce</div>
 		<nav>
 			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="shop.php">Shop</a></li>
-				<li><a href="cart.php">Cart</a></li>
-				<?php if(isset($_SESSION['user_id'])): ?>
-				<!-- If user is logged in, display the profile and logout links -->
-				<li><a href="profile.php">Profile</a></li>
-				<li><a href="logout.php">Logout</a></li>
-				<?php else: ?>
-				<!-- If user is not logged in, display the login and register links -->
-				<li><a href="login.php">Login</a></li>
-				<li><a href="register.php">Register</a></li>
-				<?php endif; ?>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Contact</a></li>
 			</ul>
 		</nav>
 	</header>
-
--- index.php
+++ index.php
@@ -12,6 +12,7 @@ include('config.php');
   <title>My Ecommerce Website</title>
 </head>
 <body>
+
   <h1>Welcome to My Ecommerce Website</h1>
   <?php if (isset($_SESSION['username'])) : ?>
     <p>Welcome, <?php echo $_SESSION['username']; ?>!</p>
@@ -20,5 +21,9 @@ include('config.php');
   <?php else : ?>
     <p><a href="login.php">Login</a> or <a href="register.php">Register</a> to start shopping.</p>
   <?php endif; ?>
+
+
+  <h2>Powered by Team leader Darshil :)  </h2>
+
 </body>
 </html>
